e74805da49c61aa6cc41ccceecf7073fcf205a5a
MartinJ <martinj50220@gmail.com>
2019-02-16 09:00:15 +0100

    fix : Corrige les annotations

--- Equida/src/main/java/com/equida/Application.java
+++ Equida/src/main/java/com/equida/Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.equida"})
-@EntityScan(basePackages = {"com.equida"})
-@EnableJpaRepositories(basePackages = {"com.equida"})
+@SpringBootApplication
+@EntityScan
+@EnableJpaRepositories
 public class Application {
 
     public static void main(String[] args) {
