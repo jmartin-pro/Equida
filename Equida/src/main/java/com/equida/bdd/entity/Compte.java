adea63517b3ab2db7ec48c76a0ec3f2510ed2e7e
MartinJ <martinj50220@gmail.com>
2019-02-17 16:52:05 +0100

    feat : Implémente l'entité Compte

---
+++ Equida/src/main/java/com/equida/bdd/entity/Compte.java
@@ -0,0 +1,46 @@
+package com.equida.bdd.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = Compte.TABLE)
+class Compte {
+	public static final String TABLE = "COMPTE";
+	
+	@Id
+	@Column(name = "ID")
+	private Long id;
+	
+	@Column(name = "LOGIN")
+	private String login;
+	
+	@Column(name = "MDP")
+	private String mdp;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+}
