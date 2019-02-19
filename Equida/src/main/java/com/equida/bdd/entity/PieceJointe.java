c2d51996d406785158a0a56e7158c0ea31dd2a92
MartinJ <martinj50220@gmail.com>
2019-02-19 10:15:57 +0100

    feat : Implémente l'entité PieceJointe

--- Equida/src/main/java/com/equida/bdd/entity/PieceJointe.java
+++ Equida/src/main/java/com/equida/bdd/entity/PieceJointe.java
@@ -1,6 +1,51 @@
 package com.equida.bdd.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = PieceJointe.TABLE)
 public class PieceJointe {
+
+	public static final String TABLE = "PIECE_JOINTE";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "CHEMIN")
+	private String chemin;
+	
+	@Column(name = "DESCRIPTION")
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getChemin() {
+		return chemin;
+	}
+
+	public void setChemin(String chemin) {
+		this.chemin = chemin;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
