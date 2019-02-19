302dbb718ad515c83b7add710e521f3e36e0de12
MartinJ <martinj50220@gmail.com>
2019-02-19 09:48:48 +0100

    feat : Implémente l'entité DirecteurGeneral

---
+++ Equida/src/main/java/com/equida/bdd/entity/DirecteurGeneral.java
@@ -0,0 +1,29 @@
+package com.equida.bdd.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = DirecteurGeneral.TABLE)
+class DirecteurGeneral {
+	public static final String TABLE = "DIRECTEUR_GENERAL";
+	
+	@Id
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ID_UTILISATEUR")
+	private Utilisateur utilisateur;
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+	
+	
+}
