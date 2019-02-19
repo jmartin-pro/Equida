747d5ed1158727436e0d2b47de889e001c8c4bca
MartinJ <martinj50220@gmail.com>
2019-02-19 10:12:00 +0100

    feat : Implémente l'entité Permission

--- Equida/src/main/java/com/equida/bdd/entity/Permission.java
+++ Equida/src/main/java/com/equida/bdd/entity/Permission.java
@@ -1,6 +1,52 @@
 package com.equida.bdd.entity;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = Permission.TABLE)
 public class Permission {
+
+	public static final String TABLE = "PERMISSION";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "LIBELLE")
+	private String ville;
+	
+	@Column(name = "DELETED")
+	private Boolean deleted;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
 
 }
