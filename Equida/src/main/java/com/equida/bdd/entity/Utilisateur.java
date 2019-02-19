95fe05ddefd84b0e69636890fff253c96d043210
MartinJ <martinj50220@gmail.com>
2019-02-19 10:28:32 +0100

    feat : Implémente l'entité Vente et Utilisateur

--- Equida/src/main/java/com/equida/bdd/entity/Utilisateur.java
+++ Equida/src/main/java/com/equida/bdd/entity/Utilisateur.java
@@ -1,6 +1,134 @@
 package com.equida.bdd.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = Utilisateur.TABLE)
 public class Utilisateur {
 
+	public static final String TABLE = "UTILISATEUR";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "NOM")
+	private String nom;
+	
+	@Column(name = "PRENOM")
+	private String prenom;
+	
+	@Column(name = "RUE")
+	private String rue;
+	
+	@Column(name = "COPOS")
+	private String copos;
+	
+	@Column(name = "VILLE")
+	private String ville;
+	
+	@Column(name = "MAIL")
+	private String mail;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ID_PAYS")
+	private Pays pays;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ID_COMPTE")
+	private Compte compte;
+	
+	@Column(name = "DELETED")
+	private Boolean deleted;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public String getCopos() {
+		return copos;
+	}
+
+	public void setCopos(String copos) {
+		this.copos = copos;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public Pays getPays() {
+		return pays;
+	}
+
+	public void setPays(Pays pays) {
+		this.pays = pays;
+	}
+
+	public Compte getCompte() {
+		return compte;
+	}
+
+	public void setCompte(Compte compte) {
+		this.compte = compte;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+	
+	
 }
