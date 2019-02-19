c50b21c880136cadab911d7d86009dd27e97faf8
MartinJ <martinj50220@gmail.com>
2019-02-19 10:10:49 +0100

    feat : Implémente l'entité Pays

---
+++ Equida/src/main/java/com/equida/bdd/entity/Pays.java
@@ -0,0 +1,52 @@
+package com.equida.bdd.entity;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = Pays.TABLE)
+public class Pays {
+
+	public static final String TABLE = "PAYS";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "LIBELLE")
+	private String ville;
+	
+	@Column(name = "DELETED")
+	private Boolean deleted;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+
+}
