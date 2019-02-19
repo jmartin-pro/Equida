fec264c40b6472d3d52d10c5136a00c2f55441d9
MartinJ <martinj50220@gmail.com>
2019-02-19 10:06:34 +0100

    feat : Implémente l'entité Lot

--- Equida/src/main/java/com/equida/bdd/entity/Lot.java
+++ Equida/src/main/java/com/equida/bdd/entity/Lot.java
@@ -1,6 +1,91 @@
 package com.equida.bdd.entity;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
-class Lot {
+@Entity
+@Table(name = Lot.TABLE)
+public class Lot {
 
+	public static final String TABLE = "LOT";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ID_CHEVAL")
+	private Cheval cheval;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ID_VENTE")
+	private Vente vente;
+	
+	@Column(name = "PRIX_DEPART")
+	private Float prixDepart;
+	
+	@Column(name = "VALIDATION")
+	private Date validation;
+	
+	@Column(name = "DELETED")
+	private Boolean deleted;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Cheval getCheval() {
+		return cheval;
+	}
+
+	public void setCheval(Cheval cheval) {
+		this.cheval = cheval;
+	}
+
+	public Vente getVente() {
+		return vente;
+	}
+
+	public void setVente(Vente vente) {
+		this.vente = vente;
+	}
+
+	public Float getPrixDepart() {
+		return prixDepart;
+	}
+
+	public void setPrixDepart(Float prixDepart) {
+		this.prixDepart = prixDepart;
+	}
+
+	public Date getValidation() {
+		return validation;
+	}
+
+	public void setValidation(Date validation) {
+		this.validation = validation;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	
 }
