3d5ed89bd0c698c93d12da42c51622afd3450e79
MartinJ <martinj50220@gmail.com>
2019-02-19 10:18:57 +0100

    feat : Implémente l'entité Role

--- Equida/src/main/java/com/equida/bdd/entity/Role.java
+++ Equida/src/main/java/com/equida/bdd/entity/Role.java
@@ -1,6 +1,51 @@
 package com.equida.bdd.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = Role.TABLE)
 public class Role {
+
+	public static final String TABLE = "ROLE";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "LIBELLE")
+	private String libelle;
+	
+	@Column(name = "DELETED")
+	private Boolean deleted;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
 
 }
