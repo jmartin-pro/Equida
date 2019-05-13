9085d1b5e534ca57996ca76fda753c526a86945b
MartinJ <martinj50220@gmail.com>
2019-05-13 23:43:41 +0200

    refactor : Simplifie rest-api.service

--- src/ionic/src/app/rest-api/rest-api.service.ts
+++ src/ionic/src/app/rest-api/rest-api.service.ts
@@ -15,27 +15,6 @@ export class RestApiService {
 	constructor(public navCtrl: NavController, private http: HttpClient) {
 	}
 
-	private handleError(error: HttpErrorResponse) {
-		if (error.error instanceof ErrorEvent) {
-			// A client-side or network error occurred. Handle it accordingly.
-			console.error('An error occurred:', error.error.message);
-		} else {
-			// The backend returned an unsuccessful response code.
-			// The response body may contain clues as to what went wrong,
-			console.error(
-				`Backend returned code ${error.status}, ` +
-				`body was: ${error.error}`
-			);
-		}
-
-		return throwError('Something bad happened; please try again later.');
-	}
-
-	private extractData(res: Response) {
-		let body = res;
-		return body || { };
-	}
-
 	public saveCredentials(username : string, passwd : string, role: string) {
 		localStorage.setItem("username", username);
 		localStorage.setItem("passwd", passwd);
@@ -46,6 +25,93 @@ export class RestApiService {
 		localStorage.removeItem("username");
 		localStorage.removeItem("passwd");
 		localStorage.removeItem("role");
+	}
+
+	checkLogin(username : string, passwd : string): Promise<any> {
+		const url = `${apiUrl}/login`;
+
+		let base64Auth = btoa(username+":"+passwd);
+		const httpOptions = {
+			headers: new HttpHeaders({'Content-Type': 'application/json',
+				'Authorization' : 'Basic '+base64Auth
+			})
+		};
+
+		//Ne pas utiliser this.execGetMethod(url) car on change le httpOptions!!
+		return this.http.get(url, httpOptions).pipe(
+			map(this.extractData),
+			catchError(this.handleError)).toPromise();
+	}
+
+	addPays(libelle: string): Promise<any> {
+		let data = {
+			libelle : libelle
+		};
+
+		const url = `${apiUrl}/pays`;
+		return this.execPostMethod(url, data);
+	}
+
+	deletePays(id: string): Promise<any> {
+		const url = `${apiUrl}/pays/${id}`;
+		return this.execDeleteMethod(url);
+	}
+
+	getChevalById(id: string): Promise<any> {
+		const url = `${apiUrl}/chevaux/${id}`;
+		return this.execGetMethod(url);
+	}
+
+	getLots(offset : number): Promise<any> {
+		const url = `${apiUrl}/lots?offset=${offset}`;
+		return this.execGetMethod(url);
+	}
+
+	getLotById(id: string): Promise<any> {
+		const url = `${apiUrl}/lots/${id}`;
+		return this.execGetMethod(url);
+	}
+
+	getPays(): Promise<any> {
+		const url = `${apiUrl}/pays`;
+		return this.execGetMethod(url);
+	}
+
+	getPaysById(id: string): Promise<any> {
+		const url = `${apiUrl}/pays/${id}`;
+		return this.execGetMethod(url);
+	}
+
+	getRaceChevalById(id: string): Promise<any> {
+		const url = `${apiUrl}/racesChevaux/${id}`;
+		return this.execGetMethod(url);
+	}
+
+	public execGetMethod(url: string): Promise<any> {
+		return this.http.get(url, this.getHttpOptions()).pipe(
+		map(this.extractData),
+		catchError(err => {
+			this.removeCredentials();
+			return this.handleError(err);
+		})).toPromise();
+	}
+
+	public execPostMethod(url: string, data: any): Promise<any> {
+		return this.http.post(url, data, this.getHttpOptions()).pipe(
+			map(this.extractData),
+			catchError(err => {
+				this.removeCredentials();
+				return this.handleError(err);
+			})).toPromise();
+	}
+
+	public execDeleteMethod(url: string): Promise<any> {
+		return this.http.delete(url, this.getHttpOptions()).pipe(
+		map(this.extractData),
+		catchError(err => {
+			this.removeCredentials();
+			return this.handleError(err);
+		})).toPromise();
 	}
 
 	private getHttpOptions() {
@@ -67,80 +133,24 @@ export class RestApiService {
 		return httpOptions;
 	}
 
-	checkLogin(username : string, passwd : string): Promise<any> {
-		const url = `${apiUrl}/login`;
+	private handleError(error: HttpErrorResponse) {
+		if (error.error instanceof ErrorEvent) {
+			// A client-side or network error occurred. Handle it accordingly.
+			console.error('An error occurred:', error.error.message);
+		} else {
+			// The backend returned an unsuccessful response code.
+			// The response body may contain clues as to what went wrong,
+			console.error(
+				`Backend returned code ${error.status}, ` +
+				`body was: ${error.error}`
+			);
+		}
 
-		let base64Auth = btoa(username+":"+passwd);
-		const httpOptions = {
-			headers: new HttpHeaders({'Content-Type': 'application/json',
-				'Authorization' : 'Basic '+base64Auth
-			})
-		};
-
-		return this.http.get(url, httpOptions).pipe(
-		map(this.extractData),
-		catchError(this.handleError)).toPromise();
+		return throwError('Something bad happened; please try again later.');
 	}
 
-	addPays(libelle: string): Promise<any> {
-		let data = {
-			libelle : libelle
-		};
-
-		const url = `${apiUrl}/pays`;
-		return this.http.post(url, data, this.getHttpOptions()).pipe(
-		map(this.extractData),
-		catchError(this.handleError)).toPromise();
-	}
-
-	deletePays(id: string): Promise<any> {
-		const url = `${apiUrl}/pays/${id}`;
-		return this.http.delete(url, this.getHttpOptions()).pipe(
-		map(this.extractData),
-		catchError(this.handleError)).toPromise();
-	}
-
-	getChevalById(id: string): Promise<any> {
-		const url = `${apiUrl}/chevaux/${id}`;
-		return this.http.get(url, this.getHttpOptions()).pipe(
-		map(this.extractData),
-		catchError(this.handleError)).toPromise();
-	}
-
-	getLots(offset : number): Promise<any> {
-		const url = `${apiUrl}/lots?offset=${offset}`;
-		console.log('url ' + url);
-		return this.http.get(url, this.getHttpOptions()).pipe(
-		map(this.extractData),
-		catchError(this.handleError)).toPromise();
-	}
-
-	getLotById(id: string): Promise<any> {
-		const url = `${apiUrl}/lots/${id}`;
-		return this.http.get(url, this.getHttpOptions()).pipe(
-		map(this.extractData),
-		catchError(this.handleError)).toPromise();
-	}
-
-	getPays(): Promise<any> {
-		const url = `${apiUrl}/pays`;
-		console.log('url ' + url);
-		return this.http.get(url, this.getHttpOptions()).pipe(
-		map(this.extractData),
-		catchError(this.handleError)).toPromise();
-	}
-
-	getPaysById(id: string): Promise<any> {
-		const url = `${apiUrl}/pays/${id}`;
-		return this.http.get(url, this.getHttpOptions()).pipe(
-		map(this.extractData),
-		catchError(this.handleError)).toPromise();
-	}
-
-	getRaceChevalById(id: string): Promise<any> {
-		const url = `${apiUrl}/racesChevaux/${id}`;
-		return this.http.get(url, this.getHttpOptions()).pipe(
-		map(this.extractData),
-		catchError(this.handleError)).toPromise();
+	private extractData(res: Response) {
+		let body = res;
+		return body || { };
 	}
 }
